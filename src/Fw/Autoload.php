220bafcd685891d39ee1e1d6da2c35d8775640a6
Mick Rippon <me@mickrippon.com>
2014-03-22 13:34:57 +1100

    Increased fail threshold for Autoload

--- src/Fw/Autoload.php
+++ src/Fw/Autoload.php
@@ -37,7 +37,7 @@ class Autoload
 				$fileName = $prefix . DIRECTORY_SEPARATOR . $fileName;
 				if (self::$debug) echo "<div>AUTOLOAD : Considering ($fileName)</div>";
 				self::$consider_count++;
-				if (self::$consider_count > 50) die("Consider Count Exceeded");
+				if (self::$consider_count > 500) die("Consider Count Exceeded");
 				if (file_exists($fileName)) {
 					if (self::$debug) echo "<div>AUTOLOAD: Returning $fileName </div>";
 					require $fileName;
