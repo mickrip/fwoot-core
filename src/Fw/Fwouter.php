23db4b0e4ca7b4364230596c972cf708e7823da2
mickrip <me@mickrippon.com>
2014-06-28 09:37:29 +1000

    Update Fwouter.php
    Refactored Fwouter

--- src/Fw/Fwouter.php
+++ src/Fw/Fwouter.php
@@ -2,29 +2,34 @@
 
 namespace Fw;
 
+
 class Fwouter
 {
 
 	static $error = "";
+	var $routes = array();
 
 	function __construct($request_method, $params = array())
 	{
 
 		//$params = array_merge($params, array("" => "index"));
 		$this->match($request_method, $params);
-
-
 	}
 
 	static function init()
 	{
 
+		// Set up defaults
+		$default_class = "Index";
+		$base_namespace = "\\Router\\";
+
 		// get request method
 		$request_method = $_SERVER['REQUEST_METHOD'];
 
 		// set request Url if it isn't passed as parameter
-		$request_url = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '/';
+		$request_url = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '/' . $default_class;
 
+		if ($request_url == "/") $request_url = "/" . $default_class;
 		// filter out query strings
 		if (($strpos = strpos($request_url, '?')) !== false) {
 			$request_url = substr($request_url, 0, $strpos);
@@ -44,26 +49,38 @@ class Fwouter
 		// Locate Router Class
 		$start = sizeof($params);
 		$debug = "";
-		do {
-			$class_name = "Router\\" . implode("\\", array_slice($params, 0, $start));
-			$debug = $debug . $class_name . "<br/>\n";
-			$start--;
 
+
+		do {
+			$class_name = $base_namespace . implode("\\", array_slice($params, 0, $start));
+			$debug = $debug . $class_name . ", \n";
+			$start--;
 		} while (!class_exists($class_name) && $start != 0);
 
-		// Check if class was found
-		if ($start == 0) {
-			self::$error = "Unable to find router handler class. <br/>\nLooked in:<br/>\n";
-			return false;
+		// Check if method in the default class (ie. /about/)
+		if (!class_exists($class_name)) {
+			// then look for the index route, and throw it into that.
+			if (class_exists($base_namespace . $default_class)) {
+				$class_name = $base_namespace . $default_class;
+				$rest_of_url = strtolower(implode("/", $params));
+				return new $class_name($request_method, $rest_of_url);
+			} else {
+				self::$error = "Unable to find router handler class. Looked for: $debug\n";
+				return false;
+			}
 		} else {
 			$rest_of_url = strtolower(implode("/", array_slice($params, $start + 1)));
 			return new $class_name($request_method, $rest_of_url);
+
 		}
+
 
 	}
 
 	function match($request_method, $params)
 	{
+		//echo "matching ($params)";
+
 		$tokens = array(
 			':string' => '([a-zA-Z]+)',
 			':number' => '([0-9]+)',
